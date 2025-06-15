ca47f97778d19f24cade6a894a88e7536d0d00ea
Krishna Sharma <kasey.gnr@icloud.com>
2025-06-15 15:04:17 +0100

    Created a sample User table in CRM database and fetched the user data using flask-mysql
    Refernce : https://stackoverflow.com/questions/9845102/using-mysql-in-flask#:~:text=Sorted%20by:,50

--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask 
+from flaskext.mysql import MySQL
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+app = Flask(__name__)
+mysql=MySQL(app)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_DB'] = 'CRM' 
+app.config['MYSQL_DATABASE_PORT'] = 8889
+
+mysql.init_app(app)
+
+conn = mysql.connect()
+if conn:
+    print("Connected to the database successfully")
+else:
+    print("Failed to connect to the database")
+
+
+
+
+@app.route('/user')
+
+def get_users():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user")
+    users = cursor.fetchall()
+    cursor.close()
+    print(users)
+    return {'users': users}
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
