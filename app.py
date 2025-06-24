cef5577a5afc593ef04354eba0d6499d44faf68e
Krishna Sharma <kasey.gnr@icloud.com>
2025-06-24 22:56:11 +0100

    init sqlite

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user
+import os
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///apartment.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
